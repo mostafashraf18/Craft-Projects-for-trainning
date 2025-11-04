cfb35f3ba0cac5f266c045bff68ea16259ea4c2b
mostafashraf18 <ma2963629@gmail.com>
2025-11-04 14:13:17 +0200

    py game basics

--- Snake Game/snake.py
+++ Snake Game/snake.py
@@ -1,1 +1,13 @@
 import pygame
+
+pygame.init()
+
+# displays surface ((Game window)) ** Top left coordinate
+screen = pygame.display.set_mode((750, 750))
+
+pygame.display.set_caption("Retro Snake")
+
+clock = pygame.time.Clock()
+
+
+# Game Loop 
