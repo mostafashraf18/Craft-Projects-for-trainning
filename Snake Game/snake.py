c5b882d93bc16ebe434aaceb396ab2bb0839668c
mostafashraf18 <ma2963629@gmail.com>
2025-11-04 14:32:51 +0200

    add the food

--- Snake Game/snake.py
+++ Snake Game/snake.py
@@ -1,4 +1,5 @@
 import pygame, sys
+from pygame.math import Vector2
 
 pygame.init()
 
@@ -6,8 +7,20 @@ pygame.init()
 GREEN = (173, 204, 96)
 DARK_GREEN = (43, 51, 24)
 
+
+## same as 750 pixels
+cell_size = 30
+number_of_cells = 254
+
+class Food:
+    def __init__(self):
+       self.position = Vector2(5,6) 
+    
+    def draw(self):
+        
+
 # displays surface ((Game window)) ** Top left coordinate
-screen = pygame.display.set_mode((750, 750))
+screen = pygame.display.set_mode((cell_size*number_of_cells, cell_size*number_of_cells))
 
 pygame.display.set_caption("Retro Snake")
 
@@ -29,4 +42,9 @@ while True:
     # 60 frame per second 
     pygame.display.update()
     clock.tick(60)
+
+    ## we made the canvas and the loop
+
+
+    ## we now will craft an invisable grid that will help us with the movement
 
