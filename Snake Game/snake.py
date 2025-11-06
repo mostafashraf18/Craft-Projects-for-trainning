ea00f429a3dddfab1e1ad8bd7e42e8ee61dc032b
mostafashraf18 <ma2963629@gmail.com>
2025-11-06 13:44:26 +0200

    gix some issues in the Food Class to support the randomness

--- Snake Game/snake.py
+++ Snake Game/snake.py
@@ -13,8 +13,8 @@ cell_size = 30
 number_of_cells = 25
 
 class Food:
-    def __init__(self):
-       self.position = Vector2(5,6) 
+    def __init__(self, snake_body):
+       self.position = self.generate_random_pos(snake_body)
     
     def draw(self):
 
@@ -27,13 +27,11 @@ class Food:
         y = random.randint(0, number_of_cells -1)
         return Vector2(x, y)
             
-    def generate_random_pos(self,):
-        x = random.randint(0, number_of_cells -1)
-        y = random.randint(0, number_of_cells -1)
-        position = Vector2(x, y)
+    def generate_random_pos(self, snake_body):
+        position = self.generate_random_cell()
 
         while position in snake_body:
-
+            position =  self.generate_random_cell()    
         return position    
 
 #snake class
