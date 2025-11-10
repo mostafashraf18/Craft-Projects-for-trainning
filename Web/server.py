da107225687903c31fdf5b9dfe797fd952d53952
mostafashraf18 <ma2963629@gmail.com>
2025-11-10 16:13:19 +0200

    add Request method

--- Web/server.py
+++ Web/server.py
@@ -1,4 +1,5 @@
 import socket
+import typing
 
 
 HOST = "127.0.0.1"
@@ -11,8 +12,65 @@ Content-length: 15
 
 <h1>Hello!</h1>""".replace(b"\n", b"\r\n")
 
-#By Default socket creates TCP sockets
 
+def iter_lines(sock: socket.socket, bufsize: int =16_384)-> typing.Generator[bytes, None, bytes]:
+   """Given a socket, read all the individual CRLF-separeted lines adn yield each one 
+   until an empty one is found. Return the remainder after the empty line."""
+
+   buff = b""
+   while True:
+      data = sock.recv(bufsize)
+      if not data:
+         return b""
+      
+      buff += data
+      while True: 
+        try:
+            i = buff.index(b"\r\n")
+            line, buff = buff[:i], buff[i + 2:]
+            if not line:
+               return buff
+            
+            yield line
+        except IndexError:
+           break
+
+#By Default socket creates TCP sockets
+class Request(typing.NamedTuple):
+    method: str
+    path: str
+    headers: typing.Mapping[str, str]
+
+    @classmethod
+    def from_socket(cls, sock: socket.socket) -> "Request":
+        """Read and parse the request from a socket object.
+
+        Raises:
+          ValueError: When the request cannot be parsed.
+        """
+        lines = iter_lines(sock)
+
+        try:
+            request_line = next(lines).decode("ascii")
+        except StopIteration:
+            raise ValueError("Request line missing.")
+
+        try:
+            method, path, _ = request_line.split(" ")
+        except ValueError:
+            raise ValueError(f"Malformed request line {request_line!r}.")
+
+        headers = {}
+        for line in lines:
+            try:
+                name, _, value = line.decode("ascii").partition(":")
+                headers[name.lower()] = value.lstrip()
+            except ValueError:
+                raise ValueError(f"Malformed header line {line!r}.")
+
+        return cls(method=method.upper(), path=path, headers=headers)
+    
+    
 with socket.socket() as server_sock:
     #This tells the kernel to reuse sockets that are in 'TIME_WAit'
     server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
@@ -28,5 +86,8 @@ with socket.socket() as server_sock:
         print(f"New Connection from {client_addr}.")
 
         with client_sock:
+          
+          for request_line in iter_lines(client_sock):
+             print(request_line)
           client_sock.sendall(RESPONSE)
 
