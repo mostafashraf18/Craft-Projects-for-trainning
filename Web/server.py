1310d32aaec47e1026b7bfb5892b1d3b38dd7e62
mostafashraf18 <ma2963629@gmail.com>
2025-11-10 16:27:28 +0200

    server finish touches

--- Web/server.py
+++ Web/server.py
@@ -1,6 +1,7 @@
 import socket
 import typing
-
+import mimetypes
+import os
 
 HOST = "127.0.0.1"
 PORT = 9000
@@ -12,6 +13,70 @@ Content-length: 15
 
 <h1>Hello!</h1>""".replace(b"\n", b"\r\n")
 
+
+SERVER_ROOT = os.path.abspath("www")
+
+FILE_RESPONSE_TEMPLATE = """\
+HTTP/1.1 200 OK
+Content-type: {content_type}
+Content-length: {content_length}
+
+""".replace("\n", "\r\n")
+
+BAD_REQUEST_RESPONSE = b"""\
+HTTP/1.1 400 Bad Request
+Content-type: text/plain
+Content-length: 11
+
+Bad Request""".replace(b"\n", b"\r\n")
+
+NOT_FOUND_RESPONSE = b"""\
+HTTP/1.1 404 Not Found
+Content-type: text/plain
+Content-length: 9
+
+Not Found""".replace(b"\n", b"\r\n")
+
+METHOD_NOT_ALLOWED_RESPONSE = b"""\
+HTTP/1.1 405 Method Not Allowed
+Content-type: text/plain
+Content-length: 17
+
+Method Not Allowed""".replace(b"\n", b"\r\n")
+
+def serve_file(sock: socket.socket, path: str) -> None:
+    """Given a socket and the relative path to a file (relative to
+    SERVER_SOCK), send that file to the socket if it exists.  If the
+    file doesn't exist, send a "404 Not Found" response.
+    """
+    if path == "/":
+        path = "/index.html"
+
+    abspath = os.path.normpath(os.path.join(SERVER_ROOT, path.lstrip("/")))
+    if not abspath.startswith(SERVER_ROOT):
+        sock.sendall(NOT_FOUND_RESPONSE)
+        return
+
+    try:
+        with open(abspath, "rb") as f:
+            stat = os.fstat(f.fileno())
+            content_type, encoding = mimetypes.guess_type(abspath)
+            if content_type is None:
+                content_type = "application/octet-stream"
+
+            if encoding is not None:
+                content_type += f"; charset={encoding}"
+
+            response_headers = FILE_RESPONSE_TEMPLATE.format(
+                content_type=content_type,
+                content_length=stat.st_size,
+            ).encode("ascii")
+
+            sock.sendall(response_headers)
+            sock.sendfile(f)
+    except FileNotFoundError:
+        sock.sendall(NOT_FOUND_RESPONSE)
+        return   
 
 def iter_lines(sock: socket.socket, bufsize: int =16_384)-> typing.Generator[bytes, None, bytes]:
    """Given a socket, read all the individual CRLF-separeted lines adn yield each one 
@@ -86,8 +151,11 @@ with socket.socket() as server_sock:
         print(f"New Connection from {client_addr}.")
 
         with client_sock:
-          
-          for request_line in iter_lines(client_sock):
-             print(request_line)
-          client_sock.sendall(RESPONSE)
+          try:
+            request = Request.from_socket(client_sock)
+            print(request)
+            client_sock.sendall(NOT_FOUND_RESPONSE)
+          except Exception as e:
+             print(f"Failed to parse request: {e}")
+             client_sock.sendall(BAD_REQUEST_RESPONSE)
 
