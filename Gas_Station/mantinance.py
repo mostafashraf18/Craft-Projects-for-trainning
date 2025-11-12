d6ce30bc82551fb4dbf32f29dd98016ae3cd8816
mostafashraf18 <ma2963629@gmail.com>
2025-11-12 10:03:53 +0200

    ADD FEATURE

--- Gas_Station/mantinance.py
+++ Gas_Station/mantinance.py
@@ -1,15 +1,25 @@
 import os
 
 
+
+service = ["Motor Check", "Replace Tires", "Add oil to the motor"]
+
 print("We Offer plenty of Mantinance services")
 print("""
       
-        1- Motor Reboot
+        1- Motor Check
         2- Replace Tires 
-        3- add oil to the motor
+        3- Add oil to the motor
         4- battery recharge 
         5- brakes check
 """)
 
-ManInput = int(input("What service you are interesting on? please writ a number \n"))
+ManInput = int(input("What service you are interesting on? please write a number \n"))
+
+if ManInput == range(1, 5):
+    print("""
+        We will do a motor check For Your Car it will cost you
+          After Taxes 500 Egp
+""")
+elif 
 
