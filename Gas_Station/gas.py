17a857decf8118324b70642a95d110cfdd87ae44
mostafashraf18 <ma2963629@gmail.com>
2025-11-12 09:42:51 +0200

    new project gas station

---
+++ Gas_Station/gas.py
@@ -0,0 +1,35 @@
+import random
+import os
+
+OCT95 = 21
+OCT90 = 19.25
+OCT80 = 17.75
+
+def get_gas_type():
+    
+    while True:
+         print("Welcome to WEEEEEE Gas Station")
+         print("""
+            95-Octane Gasoline: EGP 21.00 per liter
+            92-Octane Gasoline: EGP 19.25 per liter
+            80-Octane Gasoline: EGP 17.75 per liter
+            """)
+         user_input = int(input("what's the type of gas? please selcet number"))
+         much = int(input("How much liters You need? please type a number"))
+         if user_input == 95:
+              price = much * OCT95
+              print(f"Your Have To pay {price}")
+         elif user_input == 90:
+              price = much * OCT90
+              print(f"Your Have to pay {price}")
+         elif user_input == 80:
+              price = much * OCT80
+              print(f"Your Have to pay {price}")
+         else:
+              print("that's not A type")
+
+    
+    
+
+
+get_gas_type()
--- Rectangle_Sorter/main.py
+++ Rectangle_Sorter/main.py
@@ -5,7 +5,12 @@ import Rectangle
 
 
 
-
 settings = [line.split(": ")[1] for line in open("settings.txt", "r").read().split("\n")[:5]]
 amount = int(settings[0])
+vspeed = settings[1]
+repeat = settings[2]
+display_width = int(settings[3])
+display_height = int(settings[4])
+sorter_box_height = display_height - 100
 
+color(0, 200, 0)
